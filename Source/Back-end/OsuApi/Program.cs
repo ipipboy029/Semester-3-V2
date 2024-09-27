5f8be454869b5f46c2b360831818e2c37ba7fe05
ipipboy029 <124874752+ipipboy029@users.noreply.github.com>
2024-09-27 12:30:52 +0200

    api werkend gekregen

--- Source/Back-end/OsuApi/Program.cs
+++ Source/Back-end/OsuApi/Program.cs
@@ -8,8 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-string? clientId = builder.Configuration["Apisettings: ClientId"];
-string? clientSecret = builder.Configuration["Apisettings: ClientSecret"];
+string? clientId = builder.Configuration["ApiSettings:ClientId"];
+string? clientSecret = builder.Configuration["ApiSettings:ClientSecret"];
 builder.Services.AddSingleton((services) => new ApiService(clientId, clientSecret));
 
 var app = builder.Build();
