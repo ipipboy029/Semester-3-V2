0e31acdcfca0a7024b50a598d1f44eba6a5b378f
ipipboy029 <i.ip@student.fontys.nl>
2024-10-03 10:22:38 +0200

    CI aangepast

--- Source/Back-end/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ Source/Back-end/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5bd6bd330d845e94b64365229fe55c8bb66726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654cf16c539b52a6de28c32b8af266625f9a5087")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Source/Back-end/DataAccesslayer/obj/Debug/net8.0/DataAccesslayer.AssemblyInfo.cs
+++ Source/Back-end/DataAccesslayer/obj/Debug/net8.0/DataAccesslayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5bd6bd330d845e94b64365229fe55c8bb66726")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654cf16c539b52a6de28c32b8af266625f9a5087")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
