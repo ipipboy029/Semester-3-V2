622cf32e764389d6d339fbfa304d59a0f774d06a
ipipboy029 <124874752+ipipboy029@users.noreply.github.com>
2024-10-04 12:28:33 +0200

    sql inejction project toegevoegd

--- Source/Back-end/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ Source/Back-end/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce2940aeb7b9539d84e58695f8551d0264ebb7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ed60b2dc91228e858de7169f9a776c119654d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Source/Sql_injection/Sql_Injection/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sql_Injection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string conn = @"Data Source=LAPTOP-73JV2AJ9;Initial Catalog= Login;Integrated Security=True";
+
+            string userName = UserName.Text;
+            string password = Password.Text;
+
+            string query = "SELECT * FROM Users WHERE Username = '" + userName + "' AND Password = '" + password + "'";
+
+            using (SqlConnection connection = new SqlConnection(conn))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        MessageBox.Show("Login successful!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login failed!");
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
