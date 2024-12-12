ef14450e8ffa80864a8a1c122b6ecb3227db5440
ipipboy029 <i.ip@student.fontys.nl>
2024-12-12 12:34:49 +0100

    merge cnflict gefixed

--- Source/Back-end/BusinessLayer/Models/PlayerData.cs
+++ Source/Back-end/BusinessLayer/Models/PlayerData.cs
@@ -2,10 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-<<<<<<< HEAD
 using System.Reflection.Metadata;
-=======
->>>>>>> 6446dccd2dc1ca49272cbb20bd5a83637932babd
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,8 +25,4 @@ namespace BusinessLayer.Models
         [JsonProperty("statistics")]
         public UserStatistics userStatistics { get; set; }
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 6446dccd2dc1ca49272cbb20bd5a83637932babd
 }
--- Source/Back-end/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ Source/Back-end/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21fbdcf741abd113021bf8328a0409de8768311")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee59408392607a0b4eedfeebdcb29755c286a6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
