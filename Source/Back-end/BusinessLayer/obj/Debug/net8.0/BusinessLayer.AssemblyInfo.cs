52d8697d2fcb5d46d10351d4d7f701c85573b3ad
ipipboy029 <124874752+ipipboy029@users.noreply.github.com>
2025-01-17 11:22:32 +0100

    code coverlet toegevoegd

--- Source/Back-end/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ Source/Back-end/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc8e1a4d1380fc6dd5094def1e406f2af146344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e8b4ad1bb166a07dcaa5938011689da5ddc1450")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
